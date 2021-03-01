4185a16d399036584713bff80a5de140bc14077f
Gurleen Sethi <sarusethi@rocketmail.com>
2021-03-01 02:34:35 -0500

    feat: fix sending body to backend server in proxy request.

--- frontend/src/pages/api/[...url].ts
+++ frontend/src/pages/api/[...url].ts
@@ -1,37 +1,33 @@
 import { NextApiHandler } from "next";
 import cookie from "cookie";
 import { createProxyMiddleware } from "http-proxy-middleware";
-import axios, { AxiosError } from "axios";
-import path from "path";
 
 const { BACKEND_URL } = process.env;
 
-// const apiProxy = createProxyMiddleware("/api", {
-//   target: BACKEND_URL,
-//   pathRewrite: { "^/api/": "" },
-//   onProxyReq: (req) => {
-//     const cookieHeader = req.getHeader("cookie") as string;
-//     const parsedCookies = cookie.parse(cookieHeader || "");
-//     req.setHeader("authorization", parsedCookies["auth.access_token"] || null);
-//   },
-// });
+const apiProxy = createProxyMiddleware({
+  target: BACKEND_URL,
+  changeOrigin: true,
+  pathRewrite: { "^/api/": "" },
+  onProxyReq: (proxyReq, req, res) => {
+    const cookieHeader = proxyReq.getHeader("cookie") as string;
+    const parsedCookies = cookie.parse(cookieHeader || "");
+    proxyReq.setHeader(
+      "authorization",
+      parsedCookies["auth.access_token"] || null
+    );
+
+    const { body } = req as any;
+    if (body) {
+      const bodyData = JSON.stringify(body);
+      proxyReq.setHeader("Content-Type", "application/json");
+      proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+      proxyReq.write(bodyData);
+    }
+  },
+});
 
 const publications: NextApiHandler = async (req, res) => {
-  const { data, status, headers } = await axios({
-    method: req.method as any,
-    url: `${BACKEND_URL}${req.url.replace("/api", "")}`,
-    data: req.body,
-    headers: {
-      ...req.headers,
-      authorization: req.cookies["auth.access_token"] || null,
-    },
-  }).catch((err: AxiosError) => {
-    return err.response;
-  });
-
-  Object.keys(headers || {}).forEach((key) => res.setHeader(key, headers[key]));
-  res.statusCode = status;
-  res.json(data || {});
+  (apiProxy as any)(req, res);
 };
 
 export default publications;
